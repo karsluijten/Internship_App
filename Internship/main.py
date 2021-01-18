03c7b6b367900948a54e8dd7e83da405707de7c4
Cayah99 <48756773+Cayah99@users.noreply.github.com>
2021-01-18 17:53:31 +0100

    Update main.py

--- Internship/main.py
+++ Internship/main.py
@@ -1,7 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
 
-from bokeh.io import curdoc
+from bokeh.io import curdoc, output_notebook
 from bokeh.models.widgets import FileInput
 from bokeh.models import ColumnDataSource
+import yaml
+from bokeh.server.server import Server
+output_notebook()
+
+pd.set_option('chained_assignment', None)
+pd.options.display.float_format = '{:.4f}'.format
+import warnings
+warnings.filterwarnings('ignore')
+from bokeh.application import Application
+from bokeh.application.handlers.function import FunctionHandler
+#import fiona
+from os.path import dirname, join
 
 source = ColumnDataSource(dict())
 
